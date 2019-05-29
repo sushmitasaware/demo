4d704e053a0542ea9a475e5291d2de6e0b198a2e
sushmita <sushmita.saware@gmail.com>
2019-05-29 16:01:18 +0530

    do changes

---
+++ Page1.js
@@ -0,0 +1,10 @@
+console.log('Hello Hell!!');
+let num=100;
+console.log(`num=${num}, type=${typeof(num)}`);
+let pi= 3.14
+console.log(`pi=${pi},type=${typeof(pi)}`);
+function myfunction(){
+    console.log(`inside function`);
+    
+}
+myfunction();
